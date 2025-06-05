ded575df74ab422bdb580d4b39a6d902b594d336
lmiratrix <luke@vzvz.org>
2025-06-05 14:27:22 -0400

    contining to restructure the viz chapters.

--- case_study_code/clustered_data_simulation.R
+++ case_study_code/clustered_data_simulation.R
@@ -1,5 +1,5 @@
 
-# The final functions from the clustered data case study
+# The final functions from the clustered RCT case study
 
 library( tidyverse )
 library( lme4 )
@@ -33,7 +33,7 @@ gen_dat_model <- function( n_bar = 10,
   n_max = round( n_bar * (1 + alpha) )
   if ( n_max == n_min ) {
     if ( alpha > 0 ) {
-      warning( "alpha is too small given n_bar, making there be no variation in site size" )
+      warning( "alpha > 0 has no effect when there is no variation in site size" )
     }
     nj = rep( n_min, J )
   } else {
@@ -49,8 +49,8 @@ gen_dat_model <- function( n_bar = 10,
   
   # Calculate site intercept for each site
   beta_0j = gamma_0 + gamma_1 * Zj + gamma_2 * Zj * (nj-n_bar)/n_bar + u0j
-
-    # Make individual site membership
+  
+  # Make individual site membership
   sid = as.factor( rep( 1:J, nj ) )
   dd = data.frame( sid = sid )
   
@@ -73,19 +73,35 @@ gen_dat_model <- function( n_bar = 10,
 #### Analysis functions #####
 
 
+quiet_lmer = purrr::quietly( lmer )
+
+quiet_analysis_MLM <- function( dat ) {
+  M1 = quiet_lmer( Yobs ~ 1 + Z + (1|sid),
+                   data=dat )
+  message1 = ifelse( length( M1$message ) > 0, 1, 0 )
+  warning1 = ifelse( length( M1$warning ) > 0, 1, 0 )
+  M1 = M1$result
+  
+  est = fixef( M1 )[["Z"]]
+  se = se.fixef( M1 )[["Z"]]
+  pv = summary(M1)$coefficients["Z",5]
+  tibble( ATE_hat = est, SE_hat = se, p_value = pv,
+          message = message1, warning = warning1 )
+}
+
 analysis_MLM <- function( dat ) {
   M1 = lmer( Yobs ~ 1 + Z + (1|sid),
-             data=dat )
+                   data=dat )
+  
   est = fixef( M1 )[["Z"]]
   se = se.fixef( M1 )[["Z"]]
   pv = summary(M1)$coefficients["Z",5]
   tibble( ATE_hat = est, SE_hat = se, p_value = pv )
 }
 
-
 analysis_OLS <- function( dat ) {
   M2 <- lm_robust( Yobs ~ 1 + Z, 
-            data=dat, clusters=sid )
+                   data=dat, clusters=sid )
   est <- M2$coefficients[["Z"]]
   se  <- M2$std.error[["Z"]]
   pv <- M2$p.value[["Z"]]
@@ -113,6 +129,7 @@ analysis_agg <- function( dat ) {
 }
 
 
+
 analyze_data = function( dat ) {
   MLM = analysis_MLM( dat )
   LR = analysis_OLS( dat )
@@ -123,25 +140,38 @@ analyze_data = function( dat ) {
 }
 
 
+quiet_analyze_data = function( dat ) {
+  MLM = quiet_analysis_MLM( dat )
+  LR = analysis_OLS( dat )
+  Agg = analysis_agg( dat )
+  LR$message = 0
+  LR$warning = 0
+  Agg$message = 0
+  Agg$warning = 0
+  bind_rows( MLM = MLM, LR = LR, Agg = Agg,
+             .id = "method" )
+}
+
+
 run_CRT_sim <- function(reps, 
                         n_bar = 10, J = 30, p = 0.5,
                         ATE = 0, ICC = 0.4,
                         size_coef = 0, alpha = 0,
                         seed = NULL, aggregate = TRUE) {
-
+  
   stopifnot( ICC >= 0 && ICC < 1 )
   
   scat( "Running n=%d, J=%d, ICC=%.2f, ATE=%.2f (%d replicates)\n", n_bar, J, ICC, ATE, reps)
-
+  
   if (!is.null(seed)) set.seed(seed)
-
+  
   res <- 
     purrr::rerun( reps, {
       dat <- gen_dat_model( n_bar = n_bar, J = J, p = p,
                             gamma_0 = 0, gamma_1 = ATE, gamma_2 = size_coef,
                             sigma2_u = ICC, sigma2_e = 1 - ICC,
                             alpha = alpha )
-      analyze_data(dat)
+      quiet_analyze_data(dat)
     }) %>%
     bind_rows( .id="runID" )
 }
