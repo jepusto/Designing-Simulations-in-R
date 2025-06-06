8c5da5bc56615068ef42596143a16eea7ed504d4
jepusto <jepusto@gmail.com>
2025-06-05 22:12:49 -0500

    Working on checking estimation functions using theoretical properties.

--- case_study_code/analyze_cluster_RCT.R
+++ case_study_code/analyze_cluster_RCT.R
@@ -13,7 +13,10 @@ analysis_MLM <- function( dat ) {
 
 analysis_OLS <- function( dat, se_type = "CR2" ) {
   
-  M2 <- estimatr::lm_robust( Yobs ~ 1 + Z, data = dat, clusters = sid,  se_type = se_type)
+  M2 <- estimatr::lm_robust( 
+    Yobs ~ 1 + Z, data = dat, 
+    clusters = sid,  se_type = se_type
+  )
   
   tibble( 
     ATE_hat = M2$coefficients[["Z"]], 
@@ -24,21 +27,37 @@ analysis_OLS <- function( dat, se_type = "CR2" ) {
 
 analysis_agg <- function( dat, se_type = "HC2" ) {
   
-  datagg <- 
-    dat %>% 
-    dplyr::summarise( 
-      Ybar = mean( Yobs ),
-      n = n(),
-      .by = c(sid, Z)
-    )
+  datagg <- dplyr::summarise( 
+    dat,
+    Ybar = mean( Yobs ),
+    n = n(),
+    .by = c(sid, Z)
+  )
   
   stopifnot( nrow( datagg ) == length(unique(dat$sid) ) )
   
-  M3 <- estimatr::lm_robust( Ybar ~ 1 + Z, data = datagg, se_type = se_type )
+  M3 <- estimatr::lm_robust( 
+    Ybar ~ 1 + Z, data = datagg, 
+    se_type = se_type 
+  )
   
   tibble( 
     ATE_hat = M3$coefficients[["Z"]], 
     SE_hat = M3$std.error[["Z"]], 
     p_value = M3$p.value[["Z"]] 
   )
+}
+
+estimate_Tx_Fx <- function(
+    data, 
+    CR_se_type = "CR2", agg_se_type = "HC2"
+) {
+  
+  dplyr::bind_rows(
+    MLM = analysis_MLM( dat ),
+    OLS = analysis_OLS( dat, se_type = CR_se_type),
+    agg = analysis_agg( dat, se_type = agg_se_type),
+    .id = "estimator"
+  )
+  
 }
